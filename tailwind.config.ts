ec45c31a6cff2dcc299e98a0cb60e2b076d99527
Melina yessenia Hernández ñahuis <y3zh31409@gmail.com>
2026-01-30 03:53:15 +0000

    no estaba bien como estaba

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,6 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        body: ['Arial', 'Helvetica', 'sans-serif'],
-        headline: ['Arial', 'Helvetica', 'sans-serif'],
-        code: ['monospace'],
-      },
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
